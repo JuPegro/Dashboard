ada5dca7429ea4cd3a3c020922311ac7fee2474b
JuPegro <Majr1940@gmail.com>
2022-12-21 21:19:00 -0400

    Final Project

---
+++ js/orders.js
@@ -0,0 +1,38 @@
+const Orders = [
+    {
+        productName: 'POLO KNY',
+        productNumber: '85631',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'POSTER KNY',
+        productNumber: '15478',
+        paymentStatus: 'Due',
+        shipping: 'Declined'
+    },
+    {
+        productName: 'Chainsaw Man Vinil',
+        productNumber: '15789',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'Attack on Titan',
+        productNumber: '99999',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    },
+    {
+        productName: 'Dragon Ball spheres',
+        productNumber: '95789',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    },
+    {
+        productName: 'Tokyo Ghoul Figure',
+        productNumber: '87894',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    },
+]
